11aec99fd46215a752e31f2b5e07a7b4941df5e2
MiaPorter <serdusik@gmail.com>
2025-02-13 21:26:07 +0200

    first commit

---
+++ java.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let numDays = document.querySelector('#timerDays');
+    let numHours = document.querySelector('#timerHours');
+    let numMinutes = document.querySelector('#timerMinutes');
+    let numSeconds = document.querySelector('#timerSeconds');
+    let deadline = new Date('2025-03-15T23:59:59'); //конечная дата
+  
+    // Функция обновления таймера
+    let updateTimer = function() {
+        let now = new Date();
+        let diff = Math.max(0, deadline - now);
+  
+        let days = Math.floor(diff / (1000 * 60 * 60 * 24));
+        let hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
+        let minutes = Math.floor((diff / (1000 * 60)) % 60);
+        let seconds = Math.floor((diff / 1000) % 60);
+  
+        numDays.textContent = String(days).padStart(2, '0');
+        numHours.textContent = String(hours).padStart(2, '0');
+        numMinutes.textContent = String(minutes).padStart(2, '0');
+        numSeconds.textContent = String(seconds).padStart(2, '0');
+  
+        if (diff === 0) {
+            clearInterval(timeUpd);
+        }
+    };
+    updateTimer();
+    let timeUpd = setInterval(updateTimer, 1000);
+});
